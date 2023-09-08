bc141c8970f5fb75877359c30e1602c467156ea0
LiluUncharted <lerya-th@mail.ru>
2023-09-08 18:51:29 +0300

    Homework13

---
+++ script.js
@@ -0,0 +1,10 @@
+let country = 'Moldova';
+let continent = 'Europe';
+let population = 1.2;
+let isIsland = false;
+let countryDescription = `${country} is located in ${continent}, ${population} million people live here`;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(countryDescription);
